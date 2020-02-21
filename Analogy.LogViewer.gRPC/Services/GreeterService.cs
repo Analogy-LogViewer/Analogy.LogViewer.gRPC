216de058dbc6a81b213a89e55a50214fdd92be24
Lior Banai <liorbanai@gmail.com>
2020-02-21 13:50:12 +0200

    initial project

---
+++ Analogy.LogViewer.gRPC/Services/GreeterService.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace Analogy.LogViewer.gRPC
+{
+    public class GreeterService : Analogy.AnalogyBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AnalogyMessageReply> SendMessage(AnalogyMessageRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AnalogyMessageReply
+            {
+                Message = "Hello " + request.ID
+            });
+        }
+    }
+}
