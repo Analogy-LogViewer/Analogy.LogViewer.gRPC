cf38864be99f049a8880e525c1ed4103c597608a
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-08-13 19:11:20 +0300

    add images

---
+++ Analogy.LogViewer.gRPC/IAnalogy/gRPCAnalogyComponentImages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using Analogy.DataProviders.Extensions;
+using Analogy.LogViewer.gRPC.Properties;
+
+
+namespace Analogy.LogViewer.gRPC.IAnalogy
+{
+    public class gRPCAnalogyComponentImages : IAnalogyComponentImages
+    {
+        public Image GetLargeImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == gRPCFactory.Id)
+                return Resources.gRPC32x32;
+            return null;
+        }
+
+        public Image GetSmallImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == gRPCFactory.Id)
+                return Resources.gRPC16x16;
+            return null;
+        }
+
+        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+
+        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    }
+}
