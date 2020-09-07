038ea06a6dc22b2174b9d904fb01f7525422f4a5
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-09-07 19:42:05 +0300

    ..

--- Analogy.LogViewer.gRPC/IAnalogy/gRPCFactory.cs
+++ Analogy.LogViewer.gRPC/IAnalogy/gRPCFactory.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Analogy.Interfaces;
 using Analogy.Interfaces.Factories;
+using Analogy.LogViewer.gRPC.Properties;
 
 namespace Analogy.LogViewer.gRPC.IAnalogy
 {
@@ -20,5 +22,9 @@ namespace Analogy.LogViewer.gRPC.IAnalogy
         };
         public IEnumerable<string> Contributors { get; set; } = new List<string> { "Lior Banai" };
         public string About { get; set; } = "Analogy gRPC Server";
+        public Image SmallImage { get; set; } = Resources.gRPC16x16;
+        public Image LargeImage { get; set; } = Resources.gRPC32x32;
+
+
     }
 }
