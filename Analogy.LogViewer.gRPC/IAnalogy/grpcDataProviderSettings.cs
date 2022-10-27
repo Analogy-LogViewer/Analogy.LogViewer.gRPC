459ae768b57963970baf2caf8b46af05ec6c0cb2
Lior Banai <liorbanai@gmail.com>
2022-10-27 11:19:02 +0300

    update nugets

--- Analogy.LogViewer.gRPC/IAnalogy/grpcDataProviderSettings.cs
+++ Analogy.LogViewer.gRPC/IAnalogy/grpcDataProviderSettings.cs
@@ -3,16 +3,22 @@ using Analogy.LogViewer.Template;
 using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Analogy.Interfaces;
 
 namespace Analogy.LogViewer.gRPC.IAnalogy
 {
-    public class grpcDataProviderSettings : UserSettingsFactory
+    public class grpcDataProviderSettings : TemplateUserSettingsFactory
     {
         public override string Title { get; set; } = "gRPC settings";
-        public override UserControl DataProviderSettings { get; set; } = new grpcUserSettingsUC();
+        public override UserControl DataProviderSettings { get; set; }
 
         public override Guid FactoryId { get; set; } = gRPCFactory.Id;
         public override Guid Id { get; set; } = new Guid("2f366d60-fe44-484a-a86c-cbf5fe51a5e5");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+            DataProviderSettings = new grpcUserSettingsUC();
+        }
 
         public override Task SaveSettingsAsync()
         {
