aa8e954721012064f8c75b1ea81148bc8064160f
Lior Banai <liorbanai@gmail.com>
2025-11-17 18:51:40 +0200

    Add missing data provider

--- Analogy.LogViewer.gRPC.WinForms/IAnalogy/gRPCDataProviderWinForms.cs
+++ Analogy.LogViewer.gRPC.WinForms/IAnalogy/gRPCDataProviderWinForms.cs
@@ -2,6 +2,7 @@
 using Analogy.Interfaces.WinForms;
 using Analogy.Interfaces.WinForms.Factories;
 using Analogy.LogViewer.gRPC.IAnalogy;
+using Analogy.LogViewer.gRPC.SelfHosting;
 using Analogy.LogViewer.gRPC.WinForms.Properties;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,12 @@ namespace Analogy.LogViewer.gRPC.WinForms
 {
     public class gRPCDataProviderWinForms : gRPCDataProvider, IAnalogyDataProvidersFactoryWinForms
     {
+#if NET
+        public override IEnumerable<IAnalogyDataProvider> DataProviders { get; set; } = new List<IAnalogyDataProvider> { new gRPCServerClientWinForms(), new GRPCSelfHosting() };
+#else
         public  override IEnumerable<IAnalogyDataProvider> DataProviders { get; set; } = new List<IAnalogyDataProvider> { new gRPCServerClientWinForms() };
+#endif
+
         public Image? GetDataFactorySmallImage(Guid componentId)
         {
             return Resources.gRPC16x16;
